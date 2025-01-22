c7c8bcc6bfba10a2b31670559b8f9619aab3ba80
Guillaume-Gibault <guiyomeu@gmail.com>
2025-01-22 11:23:57 +0100

    Updated imdb_scrape.py

--- Utilities/imdb_scrape.py
+++ Utilities/imdb_scrape.py
@@ -1,10 +1,6 @@
 import os
-import time
-import pyperclip
-from PIL import Image
+import requests
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -27,15 +23,32 @@ class ImdbScrape:
         }
 
     def scrape(self):
-        cookies_declined = False
+        cookies_declined = False  # Init
         for actor in self.actors:
+            i = 1  # Init
             self.driver.get(self.actors[actor]["url"])
             if not cookies_declined:
                 decline_button = self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='reject-button']")))
+                self.driver.execute_script("arguments[0].scrollIntoView(true);", decline_button)
                 decline_button.click()
                 cookies_declined = True
             photos_button = self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "a.ipc-btn--on-onBase:nth-child(2)")))
-            photos_button.click()
+            self.driver.execute_script("arguments[0].scrollIntoView(true);", photos_button)
+            self.driver.execute_script("arguments[0].click();", photos_button)
+            if os.path.exists(f"../Datasets/IMDB Scrap/{actor}") is False:  # Create folder if it doesn't exist
+                os.makedirs(f"../Datasets/IMDB Scrap/{actor}")
+            image_element = self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'div.sc-7c0a9e7c-3 img')))
+            image_url = image_element.get_attribute("src")
+            response = requests.get(image_url, stream=True)
+            if response.status_code == 200:
+                image_path = os.path.join(os.getcwd(), f"../Datasets/IMDB Scrap/{actor}/{i}.jpg")
+                with open(image_path, 'wb') as file:
+                    for chunk in response.iter_content(1024):
+                        file.write(chunk)
+                print(f"Image enregistrée sous : {image_path}")
+                i += 1
+            else:
+                print("Échec du téléchargement de l'image.")
 
     def close(self):
         self.driver.quit()
